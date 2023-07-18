2a1e737ffd8522cc9069127413a873e413c73f20
mohapard <mohapard@gmail.com>
2023-07-18 15:04:20 +0300

    Add files via upload

--- testscout.py
+++ testscout.py
@@ -6,6 +6,109 @@ from dateutil.relativedelta import relativedelta
 import altair as alt
 import os
 
+
+translations = {
+    "English": {
+        "Welcome": "Welcome",
+        "Goodbye": "Goodbye",
+        "Login": "Login",
+        "Username": "Username",
+        "Password": "Password",
+        "Language": "Language",
+        "Invalid credentials": "Invalid credentials",
+        "Football Player Scouting - Add Player": "Football Player Scouting - Add Player",
+        "Personal Info": "Personal Info",
+        "Full Name": "Full Name",
+        "Gender": "Gender",
+        "Date of Birth": "Date of Birth",
+        "Nationality": "Nationality",
+        "City/Area": "City/Area",
+        "Current Club": "Current Club",
+        "Contact Number": "Contact Number",
+        "Estimated Value (in $EGP)": "Estimated Value (in $EGP)",
+        "Physical Attributes": "Physical Attributes",
+        "Height (cm)": "Height (cm)",
+        "Weight (kg)": "Weight (kg)",
+        "Preferred Foot": "Preferred Foot",
+        "Positional Data": "Positional Data",
+        "Primary Position": "Primary Position",
+        "Positions":"Positions",
+        "Secondary Positions": "Secondary Positions",
+        "Skills Data": "Skills Data",
+        "Evaluation": "Evaluation",
+        "General Comments": "General Comments",
+        "Strengths": "Strengths",
+        "Weaknesses": "Weaknesses",
+        "Injury History": "Injury History",
+        "Record Date": "Record Date",
+        "Add Player": "Add Player",
+        "Player successfully added!": "Player successfully added!",
+        "City":"City",
+        "Age":"Age",
+        "Personal Information":"معلومات شخصية",
+        "Physical Attributes":"الصفات البدنية",
+        "Positional Data":"البيانات الموضعية",
+        "Skills Ratings":"تقييمات المهارات",
+        "Skills Data":"بيانات المهارات"
+    },
+    "Arabic": {
+        "Welcome": "مرحبا",
+        "Goodbye": "وداعا",
+        "Login": "تسجيل الدخول",
+        "Username": "اسم المستخدم",
+        "Password": "كلمة المرور",
+        "Language": "اللغة",
+        "Invalid credentials": "بيانات الاعتماد غير صالحة",
+        "Football Player Scouting - Add Player": "الكشافة لاعب الكرة - إضافة لاعب",
+        "Personal Info": "معلومات شخصية",
+        "Full Name": "الاسم الكامل",
+        "Gender": "جنس",
+        "Date of Birth": "تاريخ الولادة",
+        "Nationality": "الجنسية",
+        "City/Area": "المدينة / المنطقة",
+        "Current Club": "النادي الحالي",
+        "Contact Number": "رقم الاتصال",
+        "Estimated Value (in $EGP)": "القيمة المقدرة (بالجنيه المصري)",
+        "Physical Attributes": "الصفات البدنية",
+        "Height (cm)": "الطول (سم)",
+        "Weight (kg)": "الوزن (كغ)",
+        "Preferred Foot": "القدم المفضلة",
+        "Positional Data": "البيانات الموضعية",
+        "Primary Position": "الموقع الأساسي",
+        "Secondary Positions": "المواقع الثانوية",
+        "Skills Data": "بيانات المهارات",
+        "Evaluation": "تقييم",
+        "General Comments": "تعليقات عامة",
+        "Strengths": "نقاط القوة",
+        "Positions": "المواقع",
+        "Weaknesses": "الضعف",
+        "Injury History": "تاريخ الإصابة",
+        "Record Date": "تاريخ التسجيل",
+        "Add Player": "إضافة لاعب",
+        "Player successfully added!": "تمت إضافة اللاعب بنجاح!",
+        "City": "مدينة",
+        "Age":"العمر",
+        "Personal Information":"معلومات شخصية",
+        "Physical":"الصفات البدنية",
+        "Physical Attributes":"الصفات البدنية",
+        "Positional Data":"البيانات الموضعية",
+        "Position":"البيانات الموضعية",
+        "Skills Ratings":"تقييمات المهارات",
+        "Skills Data":"بيانات المهارات",
+        "Add Player": "أضف لاعب",
+        "Player Statistics": "إحصائيات اللاعب",
+        "Data Summarization": "تلخيص البيانات",
+        "Full Table": "الجدول الكامل",
+        "Page": "صفحة"
+
+    }
+}
+
+
+
+
+
+
 AWS_ACCESS_KEY_ID = st.secrets['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = st.secrets['AWS_SECRET_ACCESS_KEY']
 
@@ -23,6 +126,10 @@ s3 = boto3.resource("s3",
                     aws_access_key_id=AWS_ACCESS_KEY_ID,
                     aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
 
+def get_string(language, english_string):
+    if language=='Arabic':
+        return translations[language][english_string]
+    else: return english_string
 
 def load_user_data():
     try:
@@ -108,21 +215,21 @@ else:
         player_data['date_of_birth'] = pd.to_datetime(player_data['date_of_birth'])
         player_data['age'] = player_data['date_of_birth'].apply(calculate_age)
 
-    pages = ["Add Player","Player Statistics", "Data Summarization","Full Table" ]
+    pages = ["Add Player/أضف لاعب", "Player Statistics/إحصائيات اللاعب", "Data Summarization/تلخيص البيانات", "Full Table/الجدول الكامل"]
     page = st.sidebar.selectbox("Page", pages)
 
     # Add all the required filters
     with st.sidebar.container():
         st.header("Filters")
-        selected_positions = st.multiselect("Position", options=positions) if 'primary_position' in player_data.columns else []
-        selected_genders = st.multiselect("Gender", options=['Male', 'Female']) if 'gender' in player_data.columns else []
-        selected_cities = st.multiselect("City", options=player_data['city_area'].unique().tolist()) if 'city_area' in player_data.columns else []
+        selected_positions = st.multiselect(get_string(st.session_state['language'],"Positions"), options=positions) if 'primary_position' in player_data.columns else []
+        selected_genders = st.multiselect(get_string(st.session_state['language'],"Gender"), options=['Male', 'Female']) if 'gender' in player_data.columns else []
+        selected_cities = st.multiselect(get_string(st.session_state['language'],"City"), options=player_data['city_area'].unique().tolist()) if 'city_area' in player_data.columns else []
         
         #selected_age = st.slider("Age", min_value=0, max_value=100, value=(player_data['age'].min(), player_data['age'].max())) if 'age' in player_data.columns else []
         #selected_age = st.slider("Age", min_value=0, max_value=100, step=1, value=(int(player_data['age'].min()), int(player_data['age'].max()))) if 'age' in player_data.columns else []
         min_age = int(player_data['age'].min()) if pd.notnull(player_data['age'].min()) else 0
         max_age = int(player_data['age'].max()) if pd.notnull(player_data['age'].max()) else 100
-        selected_age = st.slider("Age", min_value=0, max_value=100, step=1, value=(min_age, max_age)) if 'age' in player_data.columns else []
+        selected_age = st.slider(get_string(st.session_state['language'],"Age"), min_value=0, max_value=100, step=1, value=(min_age, max_age)) if 'age' in player_data.columns else []
 
     # Apply filters
     if len(player_data) > 0:
@@ -134,52 +241,52 @@ else:
     else:
         filtered_data = None
 
-    if page == "Add Player":
+    if page == "Add Player/أضف لاعب":
 
-        st.title('Football Player Scouting - Add Player')
+        st.title(get_string(st.session_state['language'],'Football Player Scouting - Add Player'))
 
         # Define empty dictionary to hold form inputs
         player = {}
         col1, col2, col3 = st.columns(3)
-        with st.expander("Personal Info"):
+        with st.expander(get_string(st.session_state['language'],"Personal Info")):
             # Personal Information
-            st.subheader('Personal Information')
-            player["name"] = st.text_input("Full Name")
-            player["gender"] = st.selectbox("Gender", ['Male', 'Female'])
-            player["date_of_birth"] = pd.to_datetime(st.date_input("Date of Birth", min_value= datetime.date(1990, 1, 1)))
-            player["nationality"] = st.text_input("Nationality", value="Egypt")
-            player["city_area"] = st.text_input("City/Area","Cairo")
-            player["current_club"] = st.text_input("Current Club")
-            player["contact_number"] = st.text_input("Contact Number")
-            player["estimated_value"] = st.number_input("Estimated Value (in $EGP)", min_value=0)
-        with st.expander("Physical"):
-            st.subheader('Physical Attributes')
-            player["height"] = st.text_input("Height (cm)")
-            player["weight"] = st.text_input("Weight (kg)")
-            player["preferred_foot"] = st.selectbox("Preferred Foot", [ 'Right','Left', 'Both'])
-        with st.expander("Position") :  
+            st.subheader(get_string(st.session_state['language'],'Personal Information'))
+            player["name"] = st.text_input(get_string(st.session_state['language'],"Full Name"))
+            player["gender"] = st.selectbox(get_string(st.session_state['language'],"Gender"), ['Male', 'Female'])
+            player["date_of_birth"] = pd.to_datetime(st.date_input(get_string(st.session_state['language'],"Date of Birth"), min_value= datetime.date(1990, 1, 1)))
+            player["nationality"] = st.text_input(get_string(st.session_state['language'],"Nationality"), value="Egypt")
+            player["city_area"] = st.text_input(get_string(st.session_state['language'],"City/Area"),"Cairo")
+            player["current_club"] = st.text_input(get_string(st.session_state['language'],"Current Club"))
+            player["contact_number"] = st.text_input(get_string(st.session_state['language'],"Contact Number"))
+            player["estimated_value"] = st.number_input(get_string(st.session_state['language'],"Estimated Value (in $EGP)"), min_value=0)
+        with st.expander(get_string(st.session_state['language'],"Physical")):
+            st.subheader(get_string(st.session_state['language'],'Physical Attributes'))
+            player["height"] = st.text_input(get_string(st.session_state['language'],"Height (cm)"))
+            player["weight"] = st.text_input(get_string(st.session_state['language'],"Weight (kg)"))
+            player["preferred_foot"] = st.selectbox(get_string(st.session_state['language'],"Preferred Foot"), [ 'Right','Left', 'Both'])
+        with st.expander(get_string(st.session_state['language'],"Position")) :  
             # Positional Data
-            st.subheader('Positional Data')
-            player["primary_position"] = st.selectbox("Primary Position", positions)
-            player["secondary_positions"] = st.multiselect("Secondary Positions", positions)
+            st.subheader(get_string(st.session_state['language'],'Positional Data'))
+            player["primary_position"] = st.selectbox(get_string(st.session_state['language'],"Primary Position"), positions)
+            player["secondary_positions"] = st.multiselect(get_string(st.session_state['language'],"Secondary Positions"), positions)
         
-        with st.expander("Skills Ratings"):
+        with st.expander(get_string(st.session_state['language'],"Skills Ratings")):
             # Skill Data
-            st.subheader('Skills Data')
+            st.subheader(get_string(st.session_state['language'],'Skills Data'))
             for skill in skills:
                 player[skill] = st.slider(skill, min_value=0, max_value=10,value=0)
 
-            st.subheader('Evaluation')
-            player["general_comments"] = st.text_input("General Comments")
-            player["strengths"] = st.text_input("Strengths")
-            player["weaknesses"] = st.text_input("Weaknesses")
-            player["injury"] = st.text_input("Injury History")
+            st.subheader(get_string(st.session_state['language'],'Evaluation'))
+            player["general_comments"] = st.text_input(get_string(st.session_state['language'],"General Comments"))
+            player["strengths"] = st.text_input(get_string(st.session_state['language'],"Strengths"))
+            player["weaknesses"] = st.text_input(get_string(st.session_state['language'],"Weaknesses"))
+            player["injury"] = st.text_input(get_string(st.session_state['language'],"Injury History"))
             player["record_date"] = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") # Record Date
             player["scouted_by"] = st.session_state['username']
 
 
         
-        if st.button('Add Player'):
+        if st.button(get_string(st.session_state['language'],'Add Player')):
             # Create an empty DataFrame with headers
 
             # Convert the player dictionary to a DataFrame
@@ -191,7 +298,7 @@ else:
             st.success("Player successfully added!")
         
 
-    elif page == "Data Summarization":
+    elif page == "Data Summarization/تلخيص البيانات":
         st.title('Football Player Scouting - Data Summarization')
 
         st.subheader('Data Summary')
@@ -236,7 +343,7 @@ else:
 
 
     # Page: Add Player
-    elif page == "Player Statistics":
+    elif page == "Player Statistics/إحصائيات اللاعب":
         st.title('Football Player Scouting - Player Statistics')
 
         st.subheader('Player List')
@@ -246,6 +353,6 @@ else:
             st.table(filtered_data.loc[indices].T)
         else:
             st.write("No player data available.")
-    elif page =="Full Table":
+    elif page == "Full Table/الجدول الكامل":
         st.write(player_data)
         
